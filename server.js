80efc4c6497e32d72ab201d6be16af866afa797c
Tejas Ekhande <tejasekhande01@gmail.com>
2024-08-13 00:43:13 +0530

    GV-9 | Refactor code

--- server.js
+++ server.js
@@ -1,52 +1,36 @@
 const express = require("express");
-const {RequestHandler} = require('express');
 const app = express();
 const cookieParser = require("cookie-parser");
 const swaggerUi = require('swagger-ui-express');
-require('dotenv').config();
-
 const authRoutes = require('./routes/Auth');
 const swaggerSpec = require('./config/swagger');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 4000;
-
 const db = require('./config/database.js');
 
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
 
+// Swagger Documentation
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.get('/docs.json', (req, res) => {
-    res.setHeader('Content-Type', 'application/json')
-    res.send(swaggerSpec)
-})
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+});
 
+// Routes
 app.use("/api/v1/auth", authRoutes);
 
-/**
- * @swagger
- * /:
- *   get:
- *     summary: Root endpoint
- *     description: Returns a success message with the server port.
- *     responses:
- *       200:
- *         description: Successful response with server start message.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                 message:
- *                   type: string
- */
-app.get("/", async (req, res) => {
-    return res.status(200).json({
-        success: true, message: `Server starts at ${PORT}`
-    })
-})
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send({
+        success: false,
+        message: "Internal Server Error"
+    });
+});
 
 app.listen(PORT, () => {
     console.log("Server Listening at ", PORT);
