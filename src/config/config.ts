865174a37466330f05f92473d79196b3739328bc
Vaishnav Ghenge <vaishnav.ghenge@noovosoft.com>
2024-09-01 13:13:32 +0000

    GV #32 | fix: sms verification

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,21 +3,13 @@ const SERVER_PORT = process.env.SERVER_PORT
     : 8000;
 
 export const config = {
-    core: {
-        serviceName: process.env.SERVICE_NAME as string,
-    },
     posgres: {
         url: process.env.POSTGRES_URL as string,
     },
     twilio: {
         accountSid: process.env.TWILIO_ACCOUNT_SID as string,
         authToken: process.env.TWILIO_AUTH_TOKEN as string,
-    },
-    sendgrid: {
-        apiKey: process.env.SENDGRID_API_KEY as string,
-        sender: process.env.SENDGRID_SENDER_EMAIL as string,
-        verificationTemplateId: process.env
-            .SENDGRID_EMAIL_VERIFICATION_TEMPLATE_ID as string,
+        service: process.env.TWILIO_SMS_SERVICE as string,
     },
     server: {
         port: SERVER_PORT,
--- src/library/SMSVerification.ts
+++ src/library/SMSVerification.ts
@@ -11,12 +11,12 @@ export enum SMSVerificationStatus {
 export async function createVerificationSMS(
     mobileNumber: string,
 ): Promise<VerificationInstance> {
-    const serviceName = config.core.serviceName;
+    const service = config.twilio.service;
     const accountSid = config.twilio.accountSid;
     const authToken = config.twilio.authToken;
     const client = twilio(accountSid, authToken);
 
-    return client.verify.v2.services(serviceName).verifications.create({
+    return client.verify.v2.services(service).verifications.create({
         channel: "sms",
         to: mobileNumber,
         locale: "en",
@@ -27,15 +27,13 @@ export async function createVerificationCheck(
     code: string,
     mobileNumber: string,
 ): Promise<VerificationCheckInstance> {
-    const serviceName = config.core.serviceName;
+    const service = config.twilio.service;
     const accountSid = config.twilio.accountSid;
     const authToken = config.twilio.authToken;
     const client = twilio(accountSid, authToken);
 
-    return await client.verify.v2
-        .services(serviceName)
-        .verificationChecks.create({
-            code: code,
-            to: mobileNumber,
-        });
+    return await client.verify.v2.services(service).verificationChecks.create({
+        code: code,
+        to: mobileNumber,
+    });
 }
