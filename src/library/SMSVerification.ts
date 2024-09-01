c3eb5f631983a2794b7ee17e6395734c76afa994
Vaishnav Ghenge <vaishnav.ghenge@noovosoft.com>
2024-09-01 11:47:22 +0000

    GV #30 | refactor: sms verification

--- src/library/SMSVerification.ts
+++ src/library/SMSVerification.ts
@@ -3,11 +3,6 @@ import { config } from "../config/config";
 import { VerificationInstance } from "twilio/lib/rest/verify/v2/service/verification";
 import { VerificationCheckInstance } from "twilio/lib/rest/verify/v2/service/verificationCheck";
 
-const serviceName = config.core.serviceName;
-const accountSid = config.twilio.accountSid;
-const authToken = config.twilio.authToken;
-const client = twilio(accountSid, authToken);
-
 export enum SMSVerificationStatus {
     APPROVED = "approved",
     PENDING = "pending",
@@ -16,6 +11,11 @@ export enum SMSVerificationStatus {
 export async function createVerificationSMS(
     mobileNumber: string,
 ): Promise<VerificationInstance> {
+    const serviceName = config.core.serviceName;
+    const accountSid = config.twilio.accountSid;
+    const authToken = config.twilio.authToken;
+    const client = twilio(accountSid, authToken);
+
     return client.verify.v2.services(serviceName).verifications.create({
         channel: "sms",
         to: mobileNumber,
@@ -27,6 +27,11 @@ export async function createVerificationCheck(
     code: string,
     mobileNumber: string,
 ): Promise<VerificationCheckInstance> {
+    const serviceName = config.core.serviceName;
+    const accountSid = config.twilio.accountSid;
+    const authToken = config.twilio.authToken;
+    const client = twilio(accountSid, authToken);
+
     return await client.verify.v2
         .services(serviceName)
         .verificationChecks.create({
