24f19374ccff0e058b4bc39ef95fb6c535d74feb
Tejas Ekhande <tejas.ekhande@noovosoft.com>
2024-09-17 12:37:26 +0530

    GV-33 | Add zod validations for add product endpoint

--- src/routes/Product.ts
+++ src/routes/Product.ts
@@ -1,11 +1,9 @@
 import express from "express";
-import upload from "../middleware/multer";
 import { uploadProductImage } from "../controllers/ImageUpload";
 import { addProduct } from "../controllers/Product";
+import { RequestSchemas, ValidateZod } from "../validation/utils";
 const router = express.Router();
 
-router.post('/images',uploadProductImage);
-
-router.post('/',addProduct);
+router.post("/", ValidateZod(RequestSchemas.product.product), addProduct);
 
 export default router;
